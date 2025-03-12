82a6b61389f99fe13af9b89d9f98c609d0073a3b
차정문 <ckwjdans4182@gmail.com>
2025-03-12 15:31:38 +0900

    refactor: 상위폴더 backend 추가
    Former-commit-id: 246fe2873b8f0534631d3facc28c449ded168bb7

--- auth-service/src/main/java/com/aioi/drawaing/authservice/auth/dto/EmailVerificationRequest.java
+++
@@ -1,4 +0,0 @@
-package com.aioi.drawaing.authservice.auth.dto;
-
-public record EmailVerificationRequest(String email, String code) {
-}
--- auth-service/src/main/java/com/aioi/drawaing/authservice/common/exception/CustomRedisException.java
+++
@@ -1,9 +0,0 @@
-package com.aioi.drawaing.authservice.common.exception;
-
-import com.aioi.drawaing.authservice.common.code.ErrorCode;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public class CustomRedisException extends RuntimeException {
-    private final ErrorCode errorCode;
-}
