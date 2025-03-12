7ecb86a60f2ab78fed593e5258079b4758393b93
김률아 <hobby519@naver.com>
2025-03-12 14:46:22 +0900

    feat: drawing server 세팅
    Former-commit-id: e95f5131761977486ed296d34cf663c34bb48a60

---
+++ backend/drawing-game-service/src/test/java/com/aioi/drawaing/drawinggameservice/DrawingGameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aioi.drawaing.drawinggameservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawingGameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
