d19fd31a6235d86c13e8b43932f53f4baa09092f
MoonSangHyeok <asntkdgur04@naver.com>
2025-03-18 15:34:47 +0900

    feat: 대기 방 목업

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+// import LobbyPage from './pages/LobbyPage';
+import GameWaitingRoom from './pages/GameWaitingRoom.tsx';
 // 나중에 다른 페이지들을 추가할 예정
 // import GamePage from './pages/GamePage';
 
@@ -9,8 +11,10 @@ const App: React.FC = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        {/* <Route path="/lobby" element={<LobbyPage />} /> */}
+        <Route path="/waiting-room/:roomId" element={<GameWaitingRoom />} />
         {/* 나중에 다른 경로들을 추가할 예정 */}
-        {/* <Route path="/game" element={<GamePage />} /> */}
+        {/* <Route path="/game/play" element={<GamePage />} /> */}
       </Routes>
     </BrowserRouter>
   );
