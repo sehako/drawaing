286a28a86f4ecb20412e050af613bafe8548e4d3
김률아 <hobby519@naver.com>
2025-03-21 15:36:46 +0900

    #S12P21A101-106 feat: 타이머 간단 로직 구현
    Former-commit-id: e8098c0d7fa4a488c38dae90be446948bed0a7fb

--- backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/drawing/application/DrawingService.java
+++ backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/drawing/application/DrawingService.java
@@ -1,7 +1,47 @@
 package com.aioi.drawaing.drawinggameservice.drawing.application;
 
+import com.aioi.drawaing.drawinggameservice.drawing.application.dto.SessionTimer;
+import com.aioi.drawaing.drawinggameservice.drawing.domain.TimeType;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Service
+@RequiredArgsConstructor
 public class DrawingService {
+    private final DrawMessagePublisher drawMessagePublisher;
+    private final Map<String, AtomicInteger> remainTime=new ConcurrentHashMap<>();; //type(session, draw)+sessionId
+    private final Map<String, ScheduledFuture<?>> scheduledFutures=new ConcurrentHashMap<>(); //type(session, draw)+sessionId
+    private final ScheduledExecutorService schedule;
+
+    public void publishSessionTimer(String roomId, String sessionId, int startTime) {
+        String key = getKey(TimeType.SESSION, sessionId);
+        remainTime.put(key, new AtomicInteger(startTime));
+
+        ScheduledFuture<?> scheduledFuture = schedule.scheduleAtFixedRate(()->{
+            int time = remainTime.get(key).decrementAndGet();
+            if(remainTime.get(key).intValue()<=0){
+                remainTime.remove(key);
+                stopTimer(key);
+            }
+            drawMessagePublisher.publishSessionTimer("/topic/session.total-timer/"+roomId+"/"+sessionId, new SessionTimer(time));
+
+        }, 0,1,TimeUnit.SECONDS);
+
+        scheduledFutures.put(key, scheduledFuture);
+    }
+
+    private String getKey(TimeType timeType, String sessionId) {
+        return timeType.name()+":"+sessionId;
+    }
+
+    private void stopTimer(String key){
+        scheduledFutures.get(key).cancel(true);
+    }
 }
---
+++ backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/drawing/application/dto/SessionTimer.java
@@ -0,0 +1,4 @@
+package com.aioi.drawaing.drawinggameservice.drawing.application.dto;
+
+public record SessionTimer(int totalTimer) {
+}
