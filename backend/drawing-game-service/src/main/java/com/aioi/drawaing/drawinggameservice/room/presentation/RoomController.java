524d4188db9f402b5706221748749ed0479a16bf
김률아 <hobby519@naver.com>
2025-03-24 14:55:46 +0900

    #S12P21A101-63 feat: 방 생성
    Former-commit-id: 013a6f1f237f1ef5792b51d22028cb9dd06b8a9e

--- backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/room/presentation/RoomController.java
+++ backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/room/presentation/RoomController.java
@@ -1,58 +1,19 @@
 package com.aioi.drawaing.drawinggameservice.room.presentation;
 
 import com.aioi.drawaing.drawinggameservice.room.application.RoomService;
-import com.aioi.drawaing.drawinggameservice.room.presentation.dto.RoomRequest;
-import java.util.Map;
-import java.util.Objects;
+import com.aioi.drawaing.drawinggameservice.room.presentation.dto.CreateRoomRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @RequiredArgsConstructor
+@RestController("/api/v1/drawing/room")
 public class RoomController {
-
     private final RoomService roomService;
-    private final SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/room.ready/{roomId}")
-    public void handleReady(@DestinationVariable String roomId,
-                            @Payload RoomRequest request) {
-        roomService.toggleReadyStatus(roomId, request.memberId());
+    @PostMapping
+    public String createRoom(CreateRoomRequest createRoomRequest) {
+        return roomService.createRoom("123",  createRoomRequest);
     }
-    @MessageMapping("/room.create{roomId}")
-    public void createRoom(@DestinationVariable String roomId,
-                           @Payload RoomRequest request) {
-        roomService.createRoom(roomId, request.memberId());
-    }
-    @MessageMapping("/room.join/{roomId}")
-    public void joinRoom(@DestinationVariable String roomId,
-                           @Payload RoomRequest request) {
-        roomService.joinRoom(roomId, request.memberId());
-    }
-    @MessageMapping("/room.start/{roomId}")
-    public void startGame(@DestinationVariable String roomId,
-                           @Payload RoomRequest request) {
-        roomService.startGame(roomId, request.memberId());
-    }
-    @MessageMapping("/room.leave/{roomId}")
-    public void leaveRoom(@DestinationVariable String roomId,
-                         @Payload RoomRequest request) {
-        roomService.leaveRoom(roomId, request.memberId());
-    }
-    // 필요한가?
-    @MessageExceptionHandler
-    public void handleException(Exception e, SimpMessageHeaderAccessor headerAccessor) {
-        messagingTemplate.convertAndSendToUser(
-                Objects.requireNonNull(headerAccessor.getSessionId()),
-                "/queue/errors",
-                Map.of("error", e.getMessage())
-        );
-    }
-
 }
