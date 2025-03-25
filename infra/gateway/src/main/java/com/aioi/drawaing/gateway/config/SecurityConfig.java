baff912f2838300e61a1494561a3ca271827e35b
오세학 <dhtpgkr1999@naver.com>
2025-03-25 15:29:44 +0900

    hotfix: add @EnableWebFluxSecurity Former-commit-id: 9b185698b19b1c96b7c394acf2fe705233a43a4b

--- infra/gateway/src/main/java/com/aioi/drawaing/gateway/config/SecurityConfig.java
+++ infra/gateway/src/main/java/com/aioi/drawaing/gateway/config/SecurityConfig.java
@@ -1,17 +1,5 @@
-package com.aioi.drawaing.gateway.config;
-
-import java.util.Arrays;
-import java.util.List;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
@@ -27,7 +15,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of("*")); // 프론트엔드 URL
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
