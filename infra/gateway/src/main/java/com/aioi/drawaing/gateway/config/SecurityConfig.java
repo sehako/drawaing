d8386f3cf8f6480ff187a64a44f59189540587f1
sehako <dhtpgkr1999@naver.com>
2025-03-25 15:34:13 +0900

    hotfix: add import
    Former-commit-id: 099a33799d6ce452c2e18f4cfc8ea65e00be0ea6

--- infra/gateway/src/main/java/com/aioi/drawaing/gateway/config/SecurityConfig.java
+++ infra/gateway/src/main/java/com/aioi/drawaing/gateway/config/SecurityConfig.java
@@ -1,3 +1,16 @@
+package com.aioi.drawaing.gateway.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
