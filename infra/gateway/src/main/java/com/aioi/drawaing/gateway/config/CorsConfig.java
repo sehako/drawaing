5ce3ca1c5eb5b689e91422149cf025906e597c37
sehako <dhtpgkr1999@naver.com>
2025-03-26 12:24:54 +0900

    hotfix: cors 설정 빈 등록

---
+++ infra/gateway/src/main/java/com/aioi/drawaing/gateway/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.aioi.drawaing.gateway.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of(
+                "http://127.0.0.1:5500",
+                "https://www.drawaing.site",
+                "http://70.12.247.64:5173",
+                "http://70.12.247.78:5500"
+        ));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
