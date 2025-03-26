55c811a2222e2ebb38e15aaa88171bc57755468c
차정문 <ckwjdans4182@gmail.com>
2025-03-26 14:26:40 +0900

    hotfix: 예외처리 로직 무조건 ok로 나가는 버그 수정

--- backend/auth-service/src/main/java/com/aioi/drawaing/authservice/common/response/ApiResponseEntity.java
+++ backend/auth-service/src/main/java/com/aioi/drawaing/authservice/common/response/ApiResponseEntity.java
@@ -2,19 +2,19 @@ package com.aioi.drawaing.authservice.common.response;
 
 import com.aioi.drawaing.authservice.common.code.ErrorCode;
 import com.aioi.drawaing.authservice.common.code.SuccessCode;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class ApiResponseEntity {
     public static <T> ResponseEntity<ApiResponse<T>> onSuccess(T result) {
-        return ResponseEntity.ok().body(new ApiResponse<>(SuccessCode.SUCCESS.name(), SuccessCode.SUCCESS.getMessage(), result));
+        return ResponseEntity.ok()
+                .body(new ApiResponse<>(SuccessCode.SUCCESS.name(), SuccessCode.SUCCESS.getMessage(), result));
     }
 
     public static <T> ResponseEntity<ApiResponse<T>> from(SuccessCode code, T result) {
-        return ResponseEntity.ok().body(new ApiResponse<>(code.name(), code.getMessage(), result));
+        return ResponseEntity.status(code.getStatus()).body(new ApiResponse<>(code.name(), code.getMessage(), result));
     }
 
     public static <T> ResponseEntity<ApiResponse<T>> onFailure(ErrorCode code) {
-        return ResponseEntity.ok().body(new ApiResponse<>(code.name(), code.getMessage(), null));
+        return ResponseEntity.status(code.getStatus()).body(new ApiResponse<>(code.name(), code.getMessage(), null));
     }
 }
