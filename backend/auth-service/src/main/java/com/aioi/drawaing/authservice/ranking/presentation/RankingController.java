e28e92edcdcf3632f3d707884fbb12254417867b
차정문 <ckwjdans4182@gmail.com>
2025-03-27 15:41:47 +0900

    hotfix: 게임 정보 저장 RequestBody로 수정
    Former-commit-id: 9d7165a28faacecc5c2d40b6ac353e502c36000a

--- backend/auth-service/src/main/java/com/aioi/drawaing/authservice/ranking/presentation/RankingController.java
+++ backend/auth-service/src/main/java/com/aioi/drawaing/authservice/ranking/presentation/RankingController.java
@@ -2,14 +2,15 @@ package com.aioi.drawaing.authservice.ranking.presentation;
 
 import com.aioi.drawaing.authservice.common.response.ApiResponseEntity;
 import com.aioi.drawaing.authservice.ranking.application.RankingService;
+import com.aioi.drawaing.authservice.ranking.presentation.request.GameResultRequest;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -25,11 +26,10 @@ public class RankingController {
     @PatchMapping()
     public ResponseEntity<?> updateRecord(
             HttpServletRequest request,
-            @RequestParam GameStatus status,
-            @RequestParam Integer score) {
+            @RequestBody GameResultRequest gameResultRequest) {
         Long memberId = Long.parseLong(request.getParameter("member-id"));
         return ApiResponseEntity.onSuccess(
-                rankingService.updateGameRecord(memberId, status, score)
+                rankingService.updateGameRecord(memberId, gameResultRequest.status(), gameResultRequest.score())
         );
     }
 }
