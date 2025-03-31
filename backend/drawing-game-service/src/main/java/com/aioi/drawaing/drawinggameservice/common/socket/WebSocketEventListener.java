5816d1caab4bd9174a2d1d6a284c0f2b3ccf477a
김률아 <hobby519@naver.com>
2025-03-31 14:48:45 +0900

    #S12P21A101-154 feat: socket 연결 로그 출력
    Former-commit-id: 650f2a123e345427cd086edb0a6eee59056b27fd

---
+++ backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/common/socket/WebSocketEventListener.java
@@ -0,0 +1,26 @@
+package com.aioi.drawaing.drawinggameservice.common.socket;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.time.LocalDateTime;
+
+@Component
+public class WebSocketEventListener {
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+        System.out.println("STOMP Connected at: " + LocalDateTime.now() + ", sessionId: " + sessionId);
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+        System.out.println("STOMP Disconnected at: " + LocalDateTime.now() + ", sessionId: " + sessionId);
+    }
+}
