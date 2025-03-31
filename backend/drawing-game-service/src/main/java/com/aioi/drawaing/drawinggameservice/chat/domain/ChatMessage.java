e2b116e85f4c2c147f61905a9cbb4785354af4a0
김률아 <hobby519@naver.com>
2025-03-31 13:42:30 +0900

    #S12P21A101-134 feat: chat message 형식 수정
    Former-commit-id: 753e6d1c17aad7a055c337e515cac0f8a1562053

--- backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/chat/domain/ChatMessage.java
+++ backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/chat/domain/ChatMessage.java
@@ -24,15 +24,15 @@ public class ChatMessage {
     @Id
     private String id;
     private String senderId; // 보내는 사람
-    private String roomId; // 방 번호
+    private String sessionId; // 방 번호
     private String message; // 채팅 메시지
     @CreatedDate
     private LocalDateTime createdAt;
 
-    public static ChatMessage createMessage(ChatMessageDto messageDto) {
+    public static ChatMessage createMessage(String sessionId, ChatMessageDto messageDto) {
         return ChatMessage.builder()
                 .senderId(messageDto.senderId())
-                .roomId(messageDto.roomId())
+                .sessionId(sessionId)
                 .message(messageDto.message())
                 .build();
     }
--- backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/chat/presentation/dto/ChatMessageDto.java
+++ backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/chat/presentation/dto/ChatMessageDto.java
@@ -9,14 +9,12 @@ import lombok.Builder;
 @Builder
 public record ChatMessageDto(
         String senderId,
-        String roomId,
         String message,
         LocalDateTime createdAt
 ) {
     public static ChatMessageDto of(ChatMessage chatMessage) {
         return ChatMessageDto.builder()
                 .senderId(chatMessage.getSenderId())
-                .roomId(chatMessage.getRoomId())
                 .message(chatMessage.getMessage())
                 .createdAt(LocalDateTime.now())
                 .build();
@@ -26,7 +24,6 @@ public record ChatMessageDto(
     public String toString() {
         return "ChatMessageDto{" +
                 "senderId='" + senderId + '\'' +
-                ", roomId='" + roomId + '\'' +
                 ", message='" + message + '\'' +
                 ", createdAt=" + createdAt +
                 '}';
