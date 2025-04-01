8aa92c543286ff2deb1bc3237804a7e2fc8e80f3
김률아 <hobby519@naver.com>
2025-04-01 17:24:22 +0900

    #S12P21A101-146 test: feign client service 명 바꾸기
    Former-commit-id: 0c8e7a1344d7c0abcb51bdbd7f81a6d57f4a4fda

--- backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/drawing/infrastructure/feign/AuthServiceClient.java
+++ backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/drawing/infrastructure/feign/AuthServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "auth-service", url="https://www.drawaing.site/service/auth")
+@FeignClient(name = "AUTH-SERVICE")//, url="https://www.drawaing.site/service/auth")
 public interface AuthServiceClient {
     @PatchMapping("/api/v1/ranking")
     AuthResponse updateRanking(@RequestBody GameResultRequest gameResultRequest);
