d13d19e853c72763350bf879dc369e75f5b7e425
sehako <dhtpgkr1999@naver.com>
2025-04-02 10:45:30 +0900

    feature: 지속적 해싱 로드 벨런싱 추가
    Former-commit-id: e6f036f624f74ce1c432f7de5a2c625c7586cafa

---
+++ infra/gateway/src/main/java/com/aioi/drawaing/gateway/config/HashingLoadBalancerConfig.java
@@ -0,0 +1,18 @@
+package com.aioi.drawaing.gateway.config;
+
+import com.aioi.drawaing.gateway.loadbalancer.HashingLoadBalancer;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@LoadBalancerClient(value = "game", configuration = HashingLoadBalancer.class)
+public class HashingLoadBalancerConfig {
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
