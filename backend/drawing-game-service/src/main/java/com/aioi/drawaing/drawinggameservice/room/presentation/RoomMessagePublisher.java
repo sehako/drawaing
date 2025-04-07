0db90f1479b18638564aec6cc9fd5e25b53db020
김률아 <hobby519@naver.com>
2025-04-07 15:07:09 +0900

    #S12P21A101-193 feat: 임시 전달 기능 추가
    Former-commit-id: 16dc29caaf373daf4c1b656bd446dd8efb98e4ac

--- backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/room/presentation/RoomMessagePublisher.java
+++ backend/drawing-game-service/src/main/java/com/aioi/drawaing/drawinggameservice/room/presentation/RoomMessagePublisher.java
@@ -4,9 +4,11 @@ import com.aioi.drawaing.drawinggameservice.room.application.dto.RoomStartInfo;
 import com.aioi.drawaing.drawinggameservice.room.domain.Room;
 import com.aioi.drawaing.drawinggameservice.room.presentation.dto.RoomInfoResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RoomMessagePublisher {
@@ -21,5 +23,10 @@ public class RoomMessagePublisher {
 
     public void publishRoomStart(String topic, RoomStartInfo roomStartInfo) {
         simpMessagingTemplate.convertAndSend(topic, roomStartInfo);
+    }
+
+    public void publishTemp(String topic, String message){
+        log.info("{}:{}", topic, message);
+        simpMessagingTemplate.convertAndSend(topic, message);
     }
 }
