1895fdc04ae5f25b2c4e8a6932659befcfed3e69
차정문 <ckwjdans4182@gmail.com>
2025-04-07 11:08:00 +0900

    !hotfix: 로그인 DTO 버그 수정
    Former-commit-id: 4dcafd9ae350c3a14b06dbfd5d0f3187cdb9c540

--- backend/auth-service/src/main/java/com/aioi/drawaing/authservice/member/application/response/MemberLoginResponse.java
+++ backend/auth-service/src/main/java/com/aioi/drawaing/authservice/member/application/response/MemberLoginResponse.java
@@ -1,17 +1,37 @@
 package com.aioi.drawaing.authservice.member.application.response;
 
-import com.aioi.drawaing.authservice.member.presentation.response.MemberResponse;
+import com.aioi.drawaing.authservice.member.domain.Member;
+import com.aioi.drawaing.authservice.oauth.domain.entity.ProviderType;
 import lombok.Builder;
 
 @Builder
 public record MemberLoginResponse(
-        MemberResponse memberResponse,
+        Long memberId,
+        String nickname,
+        String email,
+        String characterImage,
+        ProviderType providerType,
+        Integer level,
+        Integer exp,
+        Integer point,
         String AccessToken
 ) {
-    public static MemberLoginResponse of(MemberResponse memberResponse, String accessToken) {
+    public static MemberLoginResponse of(Member member, String accessToken) {
         return MemberLoginResponse.builder()
-                .memberResponse(memberResponse)
+                .memberId(member.getId())
+                .nickname(removeTrailingNumbers(member.getNickname()))
+                .email(member.getEmail())
+                .characterImage(member.getCharacterImage())
+                .providerType(member.getProviderType())
+                .level(member.getLevel())
+                .exp(member.getExp())
+                .point(member.getPoint())
                 .AccessToken(accessToken)
                 .build();
+    }
+
+    private static String removeTrailingNumbers(String input) {
+        // 정규식: 끝부분의 숫자를 제거
+        return input.replaceAll("\\d+$", "");
     }
 }
