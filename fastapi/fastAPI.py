e3705c9059fc60f777b1e65e0451a9129e682bae
yhkimx <yhkimx@naver.com>
2025-04-09 18:51:58 +0900

    이전으로 회귀

--- fastapi/fastAPI.py
+++ fastapi/fastAPI.py
@@ -8,7 +8,6 @@ from model.modeling import ModifiedMnasNet  # 모델 클래스 임포트. 필요
 from fastapi.middleware.cors import CORSMiddleware
 import base64  # 추가
 import os
-from fastapi import Form
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -32,29 +31,28 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # 모델 로드 (사전에 학습된 CNN 모델)
 model = ModifiedMnasNet(num_classes=90)  # 클래스 수를 맞춰서 초기화
-model.load_state_dict(torch.load("mnasnet10_112__79.pth", map_location=device))
+model.load_state_dict(torch.load("model/mnasnet10_112__79.pth", map_location=device))
 model.to(device)  # 모델을 GPU 또는 CPU로 이동
 model.eval()
 
 # 클래스 라벨 리스트 (실제 학습 데이터에 맞게 수정)
-class_labels = ['비행기', '개미', '사과', '도끼', '바나나', '헛간', '바구니', '박쥐', '곰', '침대', 
-                '벌', '새', '빵', '브로콜리', '빗자루', '양동이', '덤불', '나비', '선인장', '양초', '당근', '고양이', 
-                '의자', '시계', '구름', '소', '컵', '강아지', '문', '오리', '안경', '선풍기', '깃털', 
-                '울타리', '물고기', '꽃', '개구리', '기타', '포도', '수풀', '망치', '고슴도치', '말', 
-                '집', '열쇠', '사다리', '잎사귀', '전구', '달', '모기', '산', '쥐', '버섯', 
-                '양파', '땅콩', '배(과일)', '완두콩', '연필', '돼지', '파인애플', '감자', '토끼', '너구리', 
-                '비', '무지개', '갈퀴', '강', '샌드위치', '톱', '양', '삽', '달팽이', '뱀', 
-                '눈송이', '눈사람', '축구공', '거미', '별', '딸기', '태양', '백조', '탁자', '텐트', '트랙터', '나무', 
-                '트럭', '우산', '수박', '고래', '풍차']
-                
+class_labels = ['airplane', 'ant', 'apple', 'axe', 'banana', 'barn', 'basket', 'bat', 'bear', 'bed', 
+                'bee', 'bird', 'bread', 'broccoli', 'broom', 'bucket', 'bush', 'butterfly', 'cactus', 'candle', 'carrot', 'cat', 
+                'chair', 'clock', 'cloud', 'cow', 'cup', 'dog', 'door', 'duck', 'eyeglasses', 'fan', 'feather', 
+                'fence', 'fish', 'flower', 'frog', 'guitar', 'grapes', 'grass', 'hammer', 'hedgehog', 'horse', 
+                'house', 'key', 'ladder', 'leaf', 'light bulb', 'moon', 'mosquito', 'mountain', 'mouse', 'mushroom', 
+                'onion', 'peanut', 'pear', 'peas', 'pencil', 'pig', 'pineapple', 'potato', 'rabbit', 'raccoon', 
+                'rain', 'rainbow', 'rake', 'river', 'sandwich', 'saw', 'sheep', 'shovel', 'snail', 'snake', 
+                'snowflake', 'snowman', 'soccer ball', 'spider', 'star', 'strawberry', 'sun', 'swan', 'table', 'tent', 'tractor', 'tree', 
+                'truck', 'umbrella', 'watermelon', 'whale', 'windmill']
 
 # 이미지 변환 함수
 def transform_image(image_bytes, save_transformed_image=True):
     image = Image.open(BytesIO(image_bytes)).convert('RGB')  # RGBA나 RGB 이미지를 먼저 불러온 후
-    #print(f"Original Image Size: {image.size}")  # 이미지 크기 확인
+    print(f"Original Image Size: {image.size}")  # 이미지 크기 확인
 
     transform = transforms.Compose([
-        transforms.Resize((112, 112)),
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # 새로운 평균값과 표준편차
     ])
@@ -62,11 +60,11 @@ def transform_image(image_bytes, save_transformed_image=True):
     image_tensor = transform(image).unsqueeze(0)
     print(f"Transformed Image Tensor Shape: {image_tensor.shape}")  # 변환된 텐서 크기 확인
 
-    if save_transformed_image:
-        # 텐서를 PIL 이미지로 변환하여 저장
-        transformed_image = transforms.ToPILImage()(image_tensor.squeeze(0))
-        transformed_image.save("transformed_image.png")
-        print("Transformed image saved as 'transformed_image.png'")
+    #if save_transformed_image:
+    #    # 텐서를 PIL 이미지로 변환하여 저장
+    #    transformed_image = transforms.ToPILImage()(image_tensor.squeeze(0))
+    #    transformed_image.save("transformed_image.png")
+    #    print("Transformed image saved as 'transformed_image.png'")
 
     return image_tensor
 
@@ -80,11 +78,9 @@ def save_image(image_bytes, filename="image.png"):
     print(f"Image saved at {save_path}")
 
 @app.post("/predict")
-async def predict(file: UploadFile = File(...), quizWord: str = Form(...)):
+async def predict(file: UploadFile = File(...)):
     image_bytes = await file.read()
-    print(f"Received file: {file.filename}, size: {len(await file.read())}")
-    file.seek(0)  # Reset file position after reading
-    print(f"Received quizWord: {quizWord}")
+    
     # 이미지를 서버에 저장
     # save_image(image_bytes, filename="image.png")
     
@@ -95,36 +91,15 @@ async def predict(file: UploadFile = File(...), quizWord: str = Form(...)):
         outputs = model(image_tensor)
         print("Raw outputs:", outputs)
         probabilities = torch.nn.functional.softmax(outputs, dim=1)  # 확률 변환
-        top_probs, top_indices = torch.topk(probabilities, 5, dim=1)  # 상위 5개 예측
+        top_probs, top_indices = torch.topk(probabilities, 10, dim=1)  # 상위 10개 예측
 
-    # 예측 결과
     top_predictions = [
-        {"class": class_labels[idx.item()], "probability": round(prob.item(), 4)}
+        {"class": class_labels[idx.item()], "probability": round(prob.item(), 4)}  # 확률값을 포함
         for idx, prob in zip(top_indices[0], top_probs[0])
     ]
-
-    # quizWord와 비교하여 결과 결정
-    result = ""
-    correct = False
-    print("초기 : ",result, " / ", correct)
-    # quizWord가 예측 목록에 있는지 확인
-    for prediction in top_predictions:
-        if prediction["class"] == quizWord:
-            result = quizWord
-            correct = True
-            break
-
-    if not correct:
-        # quizWord와 일치하지 않으면 가장 확률이 높은 예측을 result로 설정
-        result = top_predictions[0]["class"]
-        correct = False
-    
-    print("확인 : ",result, " / ", correct)
-
-    # 결과 반환
+    print(top_predictions)
     return {
-        "result": result,
-        "correct": correct
+        "predictions": top_predictions,
     }
 
 if __name__ == "__main__":
