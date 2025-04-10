7198856431fb7b0492c8272f3988275593a08697
차정문 <ckwjdans4182@gmail.com>
2025-04-10 10:52:44 +0900

    !hotfix: 게임 결과 카프카 구독 기능 구현

--- backend/auth-service/src/main/java/com/aioi/drawaing/authservice/common/kafka/KafkaConsumerConfig.java
+++ backend/auth-service/src/main/java/com/aioi/drawaing/authservice/common/kafka/KafkaConsumerConfig.java
@@ -1,6 +1,7 @@
 package com.aioi.drawaing.authservice.common.kafka;
 
-import com.aioi.drawaing.authservice.member.domain.DeductPointEvent;
+import com.aioi.drawaing.authservice.game.infrastructure.dto.GameResultEventList;
+import com.aioi.drawaing.authservice.member.infrastructure.dto.DeductPointEvent;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -41,11 +42,67 @@ public class KafkaConsumerConfig {
     }
 
     @Bean
+    public ConsumerFactory<String, GameResultEventList> expEventConsumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "member-group");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        JsonDeserializer<GameResultEventList> deserializer = new JsonDeserializer<>(GameResultEventList.class);
+        deserializer.addTrustedPackages("com.aioi.drawaing");
+        deserializer.setTypeMapper(new DefaultJackson2JavaTypeMapper());
+
+        // ErrorHandlingDeserializer 적용
+        return new DefaultKafkaConsumerFactory<>(
+                config,
+                new StringDeserializer(),
+                deserializer
+        );
+    }
+
+    @Bean
+    public ConsumerFactory<String, GameResultEventList> rankScoreEventConsumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "ranking-group");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        JsonDeserializer<GameResultEventList> deserializer = new JsonDeserializer<>(GameResultEventList.class);
+        deserializer.addTrustedPackages("com.aioi.drawaing");
+        deserializer.setTypeMapper(new DefaultJackson2JavaTypeMapper());
+
+        // ErrorHandlingDeserializer 적용
+        return new DefaultKafkaConsumerFactory<>(
+                config,
+                new StringDeserializer(),
+                deserializer
+        );
+    }
+
+    @Bean
     public ConcurrentKafkaListenerContainerFactory<String, DeductPointEvent>
     deductPointEventListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, DeductPointEvent> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(deductPointEventConsumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, GameResultEventList>
+    expEventListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, GameResultEventList> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(expEventConsumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, GameResultEventList>
+    rankScoreEventListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, GameResultEventList> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(rankScoreEventConsumerFactory());
         return factory;
     }
 }
